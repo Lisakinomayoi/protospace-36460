db7c31acd335c704193816d4c67453e76e53c662
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-17 01:58:00 +0900

    修正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :edit, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   
   def index
