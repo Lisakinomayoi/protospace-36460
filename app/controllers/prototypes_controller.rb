60f27850c4c8aed0f01aca3a6a792bb6a8d45e57
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-17 21:23:22 +0900

    修正４

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,6 +28,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to root_path(@prototype)
+    end
   end
 
   def update
